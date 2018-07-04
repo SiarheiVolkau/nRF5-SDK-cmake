5f2dd339cb200f8ed3576776366dfcd5d17fb540
Siarhei Volkau <lis82@mail.by>
2018-07-04 11:29:13 +0300

    fix kdevelop 4.7.3 infinite open project

--- libs-resolve-deps.cmake
+++ libs-resolve-deps.cmake
@@ -13,13 +13,14 @@ include (${CMAKE_CURRENT_LIST_DIR}/external/libs-resolve-deps.cmake)
 #cache all libs list since later it will append same entries some times
 set(_NRF5_MODULES_LIST ${NRF5_MODULES_LIST})
 
-# other passes check and add dependencies
-set(FIRST_PASS OFF)
+set(FIRST_PASS OFF) # other passes check and add dependencies
 set(DEP_ADDED 1)
-while(DEP_ADDED)
+set(PASS 1) # required to break infinite loop
+while(DEP_ADDED AND PASS LESS 100)
 	set(DEP_ADDED 0)
 	include (${CMAKE_CURRENT_LIST_DIR}/components/libs-resolve-deps.cmake)
 	include (${CMAKE_CURRENT_LIST_DIR}/external/libs-resolve-deps.cmake)
+	math(EXPR PASS "${PASS} + 1")
 endwhile()
 
 # restore cached all libs list
